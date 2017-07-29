1eae3dff62c953aaa67075ecc74c7faec1834ffa
Roman <gremkil@gmail.com>
2017-07-29 15:59:19 +0300

    *init 2

---
+++ VirusWar/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace VirusWar.Controllers
+{
+    public class GameController : ApiController
+    {
+        [HttpPost]
+        [Route("~/api/session")]
+        public string CreateSession()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ VirusWar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VirusWar.WebApiApplication" Language="C#" %>
